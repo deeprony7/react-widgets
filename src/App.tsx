b570b1f0f92eca6a1dc38af2f24b4fb47bbc98ef
Shouvick <deeprony7@gmail.com>
2021-03-24 21:33:12 +0530

    set strict to false for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+
+
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end JS framework'
+  },
+  {
+    title: 'What use React?',
+    content: 'React is a much loved JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use react by using components'
+  }
+]
+
 
 const App = () => {
   return (
     <div>
-      Widgets App
+      <Accordion items={items}/>
     </div>
   )
 }
