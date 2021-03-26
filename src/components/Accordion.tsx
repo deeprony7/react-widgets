c10ce2543b23a561d16c45dae4f606a674c70f29
Shouvick <deeprony7@gmail.com>
2021-03-26 23:59:43 +0530

    building accordion component

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -2,6 +2,19 @@ import React from 'react'
   
 
 const Accordion = ( {items} ) => {
+    const renderedItems = items.map(item => {
+        return <div>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>{item.content}</p>
+            </div>
+        </div>
+    })
+
+
     return (
         <div>
             <h1>{items.length}</h1>
