b60f3316c0ea39b3d8f29053721db04174732899
Shouvick <deeprony7@gmail.com>
2021-03-28 01:51:15 +0530

    initital render search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,20 +10,31 @@ const Search = (props) => {
     const [results, setResults] = useState([])
 
     useEffect(() => {
-        if (term){
-            (async () => {
-                const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                    params: {
-                        action: 'query',
-                        list: 'search',
-                        origin: '*',
-                        format: 'json',
-                        srsearch: term
-                    },
-                })
-                   
-                setResults(data.query.search)
-            })()
+            
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                },
+            })
+            setResults(data.query.search)
+        }
+
+        if (term && !results.length) {
+            search()
+        }
+        else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    search()
+                }
+            }, 500)
+
+        return () => clearTimeout(timeoutId)
         }
     }, [term])
 
@@ -52,7 +63,7 @@ const Search = (props) => {
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
-                    <input 
+                    <input
                         type="text"
                         value={term}
                         onChange={e => setTerm(e.target.value)}
