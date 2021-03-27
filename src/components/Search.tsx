4beb00064b2288fbbbfa4a876b6bce30cc90043d
Shouvick <deeprony7@gmail.com>
2021-03-28 01:12:15 +0530

    dangerouslySetInnerHTML

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -31,7 +31,7 @@ const Search = (props) => {
                     <div className="header">
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         )
