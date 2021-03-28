0024962536bafeb198a1b3d58e48e8aa5e4bc2af
Shouvick <deeprony7@gmail.com>
2021-03-28 23:32:18 +0530

    added color as per the value

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -51,6 +51,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
                     </div>
                 </div>
             </div>
+            <p style={{ color: selected.value }}>This text is {selected.value}</p>
         </div>
     )
 }
