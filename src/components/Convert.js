16b4cc21b90562d91aed4623ba7258c9aa8d8bce
Shouvick <deeprony7@gmail.com>
2021-03-30 01:38:02 +0530

    debouncing text updates

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Convert = ({ language, text }) => {
-    const [translated, setTranslated] = useState('')
+    const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        }, 500)
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [text])
 
 
     useEffect(()=> {
         const doTranslation = async () => {
             const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
             params: {
-                q: text,
+                q: debouncedText,
                 target: language.value,
                 key: process.env.REACT_APP_KEY
             }
@@ -18,7 +29,7 @@ const Convert = ({ language, text }) => {
         }
 
         doTranslation()
-    }, [language, text])
+    }, [language, debouncedText])
 
     return (
         <div>
